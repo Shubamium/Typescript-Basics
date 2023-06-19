8591d81a2bea563ea1cd6e427fbd77a0a8fcdac1
Vincent Liussyaputra <liusvinv@gmail.com>
2023-06-20 05:25:22 +0700

    Add type and functions examples

--- public/index.js
+++ public/index.js
@@ -6,17 +6,21 @@ const hello = "Finally learning typescript";
 const age = 33;
 let isValid = true;
 console.log(hello + 3);
-let isChecked = false; /* Auto type infered, no need to  specify*/
-let variant; /* declaration only */
+let isChecked = false; //                                   * Auto type infered, no need to  specify*/
+let variant; //                                      declaration only */
 //----------- Any type
 let pien = "yooo";
-pien = 24; /* Won't produce errors when type changed */
+pien = 24; //                                                Won't produce errors when type changed */
 // let blue: number = "wwww";                               /* Compiled Error: Cannot assign string to number type*/
 // ---------- Arrays type
 let arr = [30, 30, 30]; //                        /* Only 1 types array, similar to in C# */
 let arr2 = ["meow", isValid]; //                     /* Normal array */
 let arr3 = arr2; //                                         /* Assigning reference also changes the types */
-// let arr4: [string, boolean] = [...arr2];                 /* Cannot be assigned even if it has the same value i.e destructuring doesn't work
+let arrAsserted = [
+    ...arr2,
+]; /* Different types can be asserted if the format is the same */
+// Different assertion
+let arrAssertedDiff = [...arr2];
 //  [2] Tuples
 //  Multiple types
 let arr6 = ["meow", isValid]; //         /* It can be manually set */
@@ -30,12 +34,50 @@ let rizzList = [
 //  Union
 let union = "true"; //                           Can be any number of types
 union = false;
-// Enums
+// [3] Enums
 var KeyCode;
 (function (KeyCode) {
     KeyCode[KeyCode["W"] = 0] = "W";
     KeyCode[KeyCode["A"] = 1] = "A";
     KeyCode[KeyCode["S"] = 2] = "S";
-    KeyCode[KeyCode["D"] = 3] = "D";
+    KeyCode[KeyCode["D"] = 2] = "D";
 })(KeyCode || (KeyCode = {}));
+console.log(KeyCode.W);
+console.log(KeyCode.A);
+console.log(KeyCode.S);
 console.log(KeyCode.D);
+let red = KeyCode.W; //                Assignable as types
+switch (red) {
+}
+// console.log(Object.keys(KeyCode));
+// [3] Objects
+let person = {
+    id: 2,
+    name: "Berry",
+};
+let mainUser = {
+    id: 200,
+    name: "Berrylia",
+    isMarried: false,
+};
+// [5] Functions
+// function multiply(a,b){ // Parameter has to have type declared
+//     return a + b;
+// }
+function multiply(a, b) {
+    // Return type can be auto inferred
+    return a * b;
+}
+// multiply(3, "hello"); // Error
+let multip = multiply(6, 9);
+console.log(multip);
+function eatBanana(target) {
+    target.isDed = "ayyo?"; // It automatically shows what both of them have in common. It's permissible/won't raise an error to assign wrong type to an object so be careful when using unions
+}
+let connor = {
+    id: 20,
+    isDed: false,
+};
+eatBanana(connor);
+console.log({ connor }); // Doesn't cause an error
+// connor.isDed = 'blow'; Raises an error despite value is already it
--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,12 @@ let isValid: boolean = true;
 
 console.log(hello + 3);
 
-let isChecked = false; /* Auto type infered, no need to  specify*/
-let variant: string; /* declaration only */
+let isChecked = false; //                                   * Auto type infered, no need to  specify*/
+let variant: string; //                                      declaration only */
 
 //----------- Any type
 let pien: any = "yooo";
-pien = 24; /* Won't produce errors when type changed */
+pien = 24; //                                                Won't produce errors when type changed */
 
 // let blue: number = "wwww";                               /* Compiled Error: Cannot assign string to number type*/
 
@@ -22,6 +22,14 @@ let arr: number[] = [30, 30, 30]; //                        /* Only 1 types arra
 let arr2: any[] = ["meow", isValid]; //                     /* Normal array */
 let arr3 = arr2; //                                         /* Assigning reference also changes the types */
 // let arr4: [string, boolean] = [...arr2];                 /* Cannot be assigned even if it has the same value i.e destructuring doesn't work
+
+type sample = [string, boolean];
+let arrAsserted: [string, boolean] = <sample>[
+  ...arr2,
+]; /* Different types can be asserted if the format is the same */
+
+// Different assertion
+let arrAssertedDiff: sample = [...arr2] as sample;
 
 //  [2] Tuples
 //  Multiple types
@@ -39,12 +47,82 @@ let rizzList: [string, boolean][] = [
 let union: string | boolean | [] = "true"; //                           Can be any number of types
 union = false;
 
-// Enums
+// [3] Enums
 enum KeyCode {
   W,
   A,
   S,
-  D,
+  D = 2, // Can be reassigned and Doesn't have to be unique
 }
 
+console.log(KeyCode.W);
+console.log(KeyCode.A);
+console.log(KeyCode.S);
 console.log(KeyCode.D);
+
+let red: KeyCode = KeyCode.W; //                Assignable as types
+
+switch (red) {
+}
+
+// console.log(Object.keys(KeyCode));
+
+// [3] Objects
+let person: {
+  id: number;
+  name: string; // Inline type/structure declaration
+} = {
+  id: 2,
+  name: "Berry",
+};
+
+// Above is too messy so:
+type Age = number; // Type is a variable for type template, can be any kind of types unions/tuples/object etc
+
+type Person = {
+  id: number;
+  name: string;
+  isMarried: boolean;
+};
+
+let mainUser: Person = {
+  id: 200,
+  name: "Berrylia",
+  isMarried: false,
+};
+
+// [5] Functions
+// function multiply(a,b){ // Parameter has to have type declared
+//     return a + b;
+// }
+function multiply(a: number, b: number): number {
+  // Return type can be auto inferred
+  return a * b;
+}
+
+// multiply(3, "hello"); // Error
+let multip = multiply(6, 9);
+console.log(multip);
+
+type monke = {
+  id: number;
+  isDed: boolean;
+};
+type human = {
+  name: string;
+  social: number;
+  isDed: string;
+};
+
+function eatBanana(target: human | monke): void {
+  target.isDed = "ayyo?"; // It automatically shows what both of them have in common. It's permissible/won't raise an error to assign wrong type to an object so be careful when using unions
+}
+
+let connor: monke = {
+  id: 20,
+  isDed: false,
+};
+
+eatBanana(connor);
+console.log({ connor }); // Doesn't cause an error
+// connor.isDed = 'blow'; Raises an error despite value is already it
